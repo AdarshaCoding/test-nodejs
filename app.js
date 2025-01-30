c0de88133c22552197dac3a62394512fd10043fe
AdarshaCoding <adarshapc4467@gmail.com>
2025-01-30 22:29:44 +0530

    Made calc as a module instead of just folder

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const { sum } = require("./calc/sum.js");
-const { multiply } = require("./calc/multiply.js");
+// const { sum } = require("./calc/sum.js");
+// const { multiply } = require("./calc/multiply.js");
+
+const { sum, multiply } = require("./calc");
 console.log("Entry Point... app.js");
 const a = 10;
 const b = 20;
